4807131ac8c50c3e1e409e1e82b1480d41519cb9
Alan Perez <alanp9six@gmail.com>
2019-07-29 15:42:04 -0500

    :art: Store is setup in the index file and also wrapped Provider around App component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import {reducers} from './components/store'
+import {reducers} from './store/reducers/'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
+const store = createStore(reducers,applyMiddleware(thunk,logger))
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
 <App />
 </Provider>
 

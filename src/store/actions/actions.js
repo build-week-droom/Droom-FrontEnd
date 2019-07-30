344609765ab65095632e59187b18cd051ca768c0
Alan Perez <alanp9six@gmail.com>
2019-07-29 23:45:18 -0500

    :sparkles: added company actions in reducers

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -18,5 +18,22 @@ export const addSeeker = (newSeeker) => dispatch => {
           payload: err.message,
         })
       })
+}
 
+export const ADD_COMPANY_START = 'ADD_COMPANY_START'
+export const ADD_COMPANY_SUCCESS  = 'ADD_COMPANY_SUCCESS'
+export const ADD_COMPANY_FAILURE  = 'ADD_COMPANY_FAILURE'
+
+export const addCompany = (newCompany) => dispatch => {
+  dispatch({type: ADD_COMPANY_START })
+    return axiosInstance
+      .post('/api/auth/register', newCompany)
+        .then(res => {
+          dispatch({ type: ADD_COMPANY_SUCCESS, payload:res.data})
+        }) 
+        .catch(err => {
+          dispatch({
+            type:ADD_COMPANY_FAILURE,
+            payload: err.message})
+        })
 }

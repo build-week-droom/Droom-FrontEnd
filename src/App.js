7960b37b9664536f85fac7cbbda4224cc293cfe4
Alan Perez <alanp9six@gmail.com>
2019-07-29 23:24:41 -0500

    :art: Switching up the file structure by using different forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import LoginForm from './components/LoginForm'
+import useLocalStorage from './auth/useLocalStorage'
+import {
+  Route,
+  Link
+} from "react-router-dom";
+// import PrivateRoute from './auth/PrivateRoute'
+import LoginFormFormik from './components/Login/LoginForm'
+import Register from './components/Register/Register'
 function App() {
+
+  const [storedToken, setToken] = useLocalStorage('token')
+
   return (
     <div className="App">
-      <LoginForm />
+      {/* <LoginForm /> */}
+
+      <div>
+      <Link to="/">Login</Link>
+      <Link to='/register'>Register</Link>
+      </div>
+
+      <Route exact path='/' component={LoginFormFormik} />
+      <Route path='/register' render={(props) => <LoginFormFormik {...props} setToken={setToken} />} />
     </div>
   );
 }

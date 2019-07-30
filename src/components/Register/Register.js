1fb873ce1ef8de814e9a580b9b69baa6c5dc1836
Alan Perez <alanp9six@gmail.com>
2019-07-29 23:29:42 -0500

    :art: Moved into a different folder

---
+++ src/components/Register/Register.js
@@ -0,0 +1,84 @@
+import React, {useState} from 'react'
+import { connect } from 'react-redux'
+import { addSeeker } from '../../store/actions/actions';
+
+const Register = (props) => {
+  const [initRegister, setRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+    isCompany: false
+  })
+
+  const handleChanges = (e) => {
+    setRegister({...initRegister, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addSeeker(initRegister)
+    console.log(initRegister)
+  }
+  const toggleCheck = e => {
+    console.log('toggle check')
+    return setRegister({isCompany: !props.isCompany})
+    // console.log(!props.isCompany)
+  }
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          name='name'
+          placeholder='Enter Name'
+          value={props.name}
+          onChange={handleChanges}
+        />
+        <input 
+          type='text'
+          name='email'
+          placeholder='Enter Email'
+          value={props.email}
+          onChange={handleChanges}
+        />
+        <input 
+          type='text'
+          name='password'
+          placeholder='Emter Password'
+          value={props.password}
+          onChange={handleChanges}
+        />
+        <div className='ui toggle checkbox'>
+          <label>
+            <input 
+              type='radio'
+              name='isCompany'
+              onChange={toggleCheck}
+              checked={!props.isCompany}
+              />
+              Seeker
+          </label>
+          </div>
+          <div className='ui toggle checkbox'>
+            <label>
+            
+              <input 
+                type='radio'
+                name='isCompany'
+                onChange={toggleCheck}
+                checked={props.isCompany}
+                />
+                Company
+            </label>
+          </div>
+          <button className="ui submit button" onClick={handleSubmit}>
+            Register
+          </button>
+      </form>
+    </div>
+  )
+}
+
+
+export default connect(null, {addSeeker})(Register)

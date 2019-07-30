d9df9e05dfd7bad27cd0643b5213cdad36c6499d
Tracie T <traciecodes@gmail.com>
2019-07-30 12:37:39 -0500

    WIP.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Login from './components/Login'
+import Login from './components/Login';
+import CompanyProfile from './components/CompanyProfile';
+
 function App() {
   return (
     <div className="App">
       <Login />
+      <CompanyProfile />
+
     </div>
   );
 }

779806db8d3ba02e284e9607c295c9ce6edb0e1d
Alan Perez <alanp9six@gmail.com>
2019-07-30 12:14:40 -0500

    :recycle: Swapped Seeker to USER

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,39 +1,21 @@
 import { axiosInstance } from '../../auth/helpers'
 
-export const ADD_SEEKER_START = 'ADD_SEEKER_START'
-export const ADD_SEEKER_SUCCESS  = 'ADD_SEEKER_SUCCESS'
-export const ADD_SEEKER_FAILURE  = 'ADD_SEEKER_FAILURE'
+export const ADD_USER_START = 'ADD_USER_START'
+export const ADD_USER_SUCCESS  = 'ADD_USER_SUCCESS'
+export const ADD_USER_FAILURE  = 'ADD_USER_FAILURE'
 
 
-export const addSeeker = (newSeeker) => dispatch => {
-  dispatch({ type: ADD_SEEKER_START })
+export const addUser = (newUser) => dispatch => {
+  dispatch({ type: ADD_USER_START })
     return axiosInstance
-      .post('/api/auth/register', newSeeker)
+      .post('/api/auth/register', newUser)
       .then( res => {
-        dispatch({ type: ADD_SEEKER_SUCCESS, payload:res.data})
+        dispatch({ type: ADD_USER_SUCCESS, payload:res.data})
       })
       .catch(err => {
         dispatch({
-          type: ADD_SEEKER_FAILURE,
+          type: ADD_USER_FAILURE,
           payload: err.message,
         })
       })
-}
-
-export const ADD_COMPANY_START = 'ADD_COMPANY_START'
-export const ADD_COMPANY_SUCCESS  = 'ADD_COMPANY_SUCCESS'
-export const ADD_COMPANY_FAILURE  = 'ADD_COMPANY_FAILURE'
-
-export const addCompany = (newCompany) => dispatch => {
-  dispatch({type: ADD_COMPANY_START })
-    return axiosInstance
-      .post('/api/auth/register', newCompany)
-        .then(res => {
-          dispatch({ type: ADD_COMPANY_SUCCESS, payload:res.data})
-        }) 
-        .catch(err => {
-          dispatch({
-            type:ADD_COMPANY_FAILURE,
-            payload: err.message})
-        })
 }

536f25a51b69cf29f351a8b382f74b77fed0d3f7
jazz-code <ren.meta@gmail.com>
2019-07-31 16:12:09 -0400

    added companyLinks map and seekerLinks map

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../../stylesheets/components/navbar.scss";
 
-const Navbar = ({ links }) => {
+const Navbar = ({ companyLinks, seekerLinks }) => {
   return (
-    <nav className="navbar">
-      {links.map((l, idx) => (
-        <NavLink key={idx} to={`/${l.toLowerCase()}/`}>
-          {l}
-        </NavLink>
-      ))}
-    </nav>
+    <div>
+      <nav className="navbar-companies">
+        {companyLinks.map((l, idx) => (
+          <NavLink key={idx} to={`/${l.toLowerCase()}/`}>
+            {l}
+          </NavLink>
+        ))}
+      </nav>
+      <nav className="navbar-seekers">
+        {seekerLinks.map((l, idx) => (
+          <NavLink key={idx} to={`/${l.toLowerCase()}/`}>
+            {l}
+          </NavLink>
+        ))}
+      </nav>
+    </div>
   );
 };
 export default Navbar;

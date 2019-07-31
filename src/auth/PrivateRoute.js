1aaf19f153f179659c66088efb73abcc17b2e250
Alan Perez <alanp9six@gmail.com>
2019-07-31 14:30:53 -0500

    :bug: checks if user is authenticated.. if not redirect to login page

--- src/auth/PrivateRoute.js
+++ src/auth/PrivateRoute.js
@@ -4,30 +4,50 @@ import {decodeToken} from '../auth/Token'
 import { getToken } from './localStorage';
 import roles from './roles'
 import routes from './routes'
-const PrivateRoute = ({ component: Component, ...rest }) => (
-<Route
-    {...rest}
-    render={props => {
-      const currentUser = getToken();
-      const user = decodeToken(currentUser);
-      // const {isCompany} = decodeToken()
-      console.log('clg user: ', user)
-      console.log('roles.isCompanmy: ', roles.isCompany)
-      console.log('PRIVATE ROUTE DECODETOKEN:  ', decodeToken(currentUser))
-      console.log('clg user.isco:  ', user.isCompany)
-      if (!currentUser ) {
-        return <Redirect to={routes.login} />;
-      } else if(user.isCompany === roles.isCompany) {
-        return <Redirect to={routes.profile} />
-      }
-      // if (user.role !== roles.company) {
-      //     return <Redirect to={routes.home}/>;
-      //   }
+// const PrivateRoute = ({ component: Component, ...rest }) => (
+// <Route
+//     {...rest}
+//     render={props => {
+//       const currentUser = getToken();
+//       const user = decodeToken(currentUser);
+     
+//       if (!currentUser ) {
+//         console.log('CLG ROUTES.LOGIN IN PRVTRT: ',routes.login)
+//         return <Redirect to='/login' />;
+//       } else if(user.isCompany === roles.isCompany) {
+//         return <Redirect to='/profile' />
+//       }
+//       // if (user.role !== roles.company) {
+//       //     return <Redirect to={routes.home}/>;
+//       //   }
       
 
-      // return <Component {...props} />;
-    }}
-  />
-);
+//       // return <Component {...props} />;
+//     }}
+//   />
+// );
 
+// 
+
+
+function PrivateRoute({ component: Component, ...rest }) {
+  const currentUser = getToken();
+  const user = decodeToken(currentUser);
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        user ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login"
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 export default PrivateRoute

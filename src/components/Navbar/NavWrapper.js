399c65a0ce4c56d767819f2a228f3ea561f80d60
jazz-code <ren.meta@gmail.com>
2019-07-31 15:40:07 -0400

    added some styling to Navbar.scss in stylesheets

--- src/components/Navbar/NavWrapper.js
+++ src/components/Navbar/NavWrapper.js
@@ -1,5 +1,8 @@
 import React from "react";
+import "../../stylesheets/components/navbar.scss";
 
 const NavWrapper = ({ children }) => {
   return <header className="navigation">{children}</header>;
 };
+
+export default NavWrapper;

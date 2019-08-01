7c07c54fbf965810b2b30ee3034aef8734263286
Tracie T <traciecodes@gmail.com>
2019-08-01 09:50:30 -0500

    Added companycard.scss

--- src/components/Jobs/CompanyCard/CompanyCard.js
+++ src/components/Jobs/CompanyCard/CompanyCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Image, Icon } from 'semantic-ui-react'
 
 export default function CompanyCard({ company }) {
-  const { name, email, about, location, profileImg } = company
+  const { name, email, about, location } = company
 
   const logoUrl = {
     Andela: 'https://cutlerpr.co/images/case-studies/andela-logo.png',
@@ -20,22 +20,65 @@ export default function CompanyCard({ company }) {
       'https://pbs.twimg.com/profile_images/810741743436124160/sfGjeR7F_400x400.jpg'
   }
 
+
   // Get / set company logo
   let logo = ''
   if (logoUrl.hasOwnProperty(company)) {
     logo = logoUrl[company]
   }
 
+  let jobCardHighlight = ''
+  switch (company) {
+    case 'Andela':
+      jobCardHighlight = 'teal'
+      break
+    case 'Epic Flight Academy':
+      jobCardHighlight = 'purple'
+      break
+    case 'GE':
+      jobCardHighlight = 'blue'
+      break
+    case 'Impulsion Marketing Group':
+      jobCardHighlight = 'yellow'
+      break
+    case 'La Causa, Inc':
+      jobCardHighlight = 'red'
+      break
+    case 'LaserCycle USA':
+      jobCardHighlight = 'green'
+      break
+    case 'Paystack Nigeria':
+      jobCardHighlight = 'violet'
+      break
+    default:
+      jobCardHighlight = 'gray'
+  }
+  
   return (
-    <Card className="company-card">
-        <Image className="job-card-image" src={logo} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-        <Card.Content>{about}</Card.Content>
-        <Card.Meta>{location}</Card.Meta>
-        <Card.Meta>{email}</Card.Meta>
+    <Card className="company-card" color={jobCardHighlight}>
+      <Image className="job-card-image" src={logo} wrapped ui={false} />
+      <Card.Content className="job-card-main-content">
+        <Card.Header className="job-card-company">{name}</Card.Header>
+        <Card.Header className="job-card-location">{location}</Card.Header>
+        <Card.Meta>
+          <span className="job-card-location">{email}</span>
+        </Card.Meta>
+        <Card.Description className="job-card-description">
+          {about}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra className="job-card-selection">
+        <a>
+          <Icon name="user" />
+          Yeah Baby!
+        </a>
+        <a>
+          <Icon name="user" />
+          Hell No!
+        </a>
       </Card.Content>
     </Card>
-
   )
 }
+
+
